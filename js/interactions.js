aa481b3cd32f25d592ca7dbc59122c7cf8417fba
dingocat <wizomagicland@gmail.com>
2014-11-12 12:10:34 -0500

    website page layout

--- js/interactions.js
+++ js/interactions.js
@@ -1,3 +1,11 @@
 $(function() {
     $( ".draggable" ).draggable();
  });
+$('.clickable').click(function () {
+            $(this).css('z-index', '500');
+            $('.clickable').not(this).css('z-index', '1');
+        });
+$('.clickableSite').click(function () {
+	$('.clickableSite:second-child').css('opacity', '0');
+	$('.clickableSite:second-child').not(this).css('opacity', '1');
+});
