d3d197f341814e582150d967e784d52f4eaab1c0
dingocat <wizomagicland@gmail.com>
2014-11-13 18:20:37 -0500

    jQuizzle toggle text.

--- js/interactions.js
+++ js/interactions.js
@@ -5,7 +5,15 @@ $('.clickable').click(function () {
             $(this).css('z-index', '500');
             $('.clickable').not(this).css('z-index', '1');
         });
-$('.clickableSite').click(function () {
-	$('.clickableSite:second-child').css('opacity', '0');
-	$('.clickableSite:second-child').not(this).css('opacity', '1');
+$('.site').hover(function () {
+            $(this).css('z-index', '500');
+            $('.site').not(this).css('z-index', '1');
+        });
+$('#exohelixsite').click(function() {
+	$('.descriptionExo').css('opacity', '1');
+	$('.descriptionArs').css('opacity', '0');
 });
+$('#arsllc').click(function () {
+	$('.descriptionArs').css('opacity', '1');
+	$('.descriptionExo').css('opacity', '0');
+})
